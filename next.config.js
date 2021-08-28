4c4ea805c1565e7d5df4d26fdbd7f60d651f7718
Narek Danielyan <danielyan.narek99@gmail.com>
2021-08-28 17:17:54 +0400

    testing map method

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/omOss',
-        destination: '/about',
-      },
-      {
-        source: '/artiklar',
-        destination: '/articles',
-      },
-    ]
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/omOss',
+  //       destination: '/about',
+  //     },
+  //     {
+  //       source: '/artiklar',
+  //       destination: '/articles',
+  //     },
+  //   ]
+  // },
+  exportPathMap: async function (
+  ) {
+    return {
+      '/omOss': { page: '/about' },
+      '/artiklar': { page: '/articles' },
+    };
   },
+
 }
