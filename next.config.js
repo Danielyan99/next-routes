90e5bf8907b0435478d1f396d7f6bf394e4ecdd8
Narek Danielyan <danielyan.narek99@gmail.com>
2021-08-28 17:47:49 +0400

    a

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 module.exports = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/omOss',
-  //       destination: '/about',
-  //     },
-  //     {
-  //       source: '/artiklar',
-  //       destination: '/articles',
-  //     },
-  //   ]
-  // },
-  exportPathMap: async function (
-  ) {
-    return {
-      '/omOss': { page: '/about' },
-      '/artiklar': { page: '/articles' },
-    };
+  async rewrites() {
+    return [
+      {
+        source: '/omOss',
+        destination: '/about',
+      },
+      {
+        source: '/artiklar',
+        destination: '/articles',
+      },
+    ]
   },
 
 }
